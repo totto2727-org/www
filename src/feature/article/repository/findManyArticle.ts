a8838c195959b98dcd0f1403c94f44a422003c09
totto2727 <h_tsuchida@yumemi.co.jp>
2024-12-07 02:22:28 +0900

    style: apply

--- src/feature/article/repository/findManyArticle.ts
+++ src/feature/article/repository/findManyArticle.ts
@@ -7,9 +7,9 @@ export function findManyArticle() {
 			article.id.startsWith("article") && article.id !== "article.mdx",
 	).then((articles) => {
 		return articles
-		.filter((article) => article.data.draft === false)
-		.toSorted((a, b) => {
-			return a.id < b.id ? 1 : -1
-		})
+			.filter((article) => article.data.draft === false)
+			.toSorted((a, b) => {
+				return a.id < b.id ? 1 : -1
+			})
 	})
 }
